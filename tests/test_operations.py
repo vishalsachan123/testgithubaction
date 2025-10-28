02395dc017a8364a5f4d06259637599134987b0f
Vishal Sachan <mrvishalpatel673@gmail.com>
2025-10-28 20:25:20 +0530

    ffg

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,7 +4,9 @@ from src.math_operations import add ,sub
 def test_add():
     assert add(2,3) == 5
     assert add(6,3) == 9
+    print('add success')
 
 def test_sub():
     assert sub(5,4) == 1
     assert sub(5,6) == -1
+    print('substraction success')
